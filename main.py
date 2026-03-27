8630a0ef757939cdbd56aace44f3f26a06a41c7c
sheng11-eleven <veesheng123@gmail.com>
2026-03-27 14:56:01 +0800

    python

---
+++ main.py
@@ -0,0 +1,6 @@
+from Methods import *
+
+if __name__ == "__main__":
+    server()  # creating Server and tables
+    print("\nBajaj Finance - Online Insurance\n")
+    login_input()  # initial login
